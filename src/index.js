13792268033a05454c057ccf13cea6c6fa1ba3e3
vivianeaco@yahoo.com.br <vivianeaco@yahoo.com.br>
2024-08-13 18:23:16 -0300

    Primeiras linhas do código

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 let nomeHeroi = "Viviane"
-let nivelHeroi = "88"
+let xpHeroi = 999;
 
-switch (nivelHeroi) {
-    case (nivelHeroi >= 1000 && nivelHeroi = "Ferro")
+let nivelHeroi;
+
+if (xpHeroi < 1000) {
+    nivelHeroi = "Ferro";
+} else if (xpHeroi >= 1001 && xpHeroi <= 2000) {
+    nivelHeroi = "Bronze";
 }
+
+
